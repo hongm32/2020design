9f2fa4ad77e58fd4f53bd2ee6a6496252e09a96e
hongm32 <XHM,1024cj>
2020-11-12 10:39:05 +0800

    修改《【第09课】枚举算法》课件、源码及相关资料

--- 【第09课】枚举算法/Q3火柴棒.py
+++ 【第09课】枚举算法/Q3火柴棒.py
@@ -6,15 +6,19 @@ def match_num(num):
         total = 0
     while num > 0:
         x = num % 10  # 取num除以10的余数，即num的个位数
-        total = total + f[x]  # 所需火柴棒数累加
-        num = num // 10	 # num整除10，即去掉num的个位数
+        total += f[x]  # 所需火柴棒数累加
+        num //= 10	 # num整除10，即去掉num的个位数
     return total  # 返回需要多少根火柴棒数
 
 
-# 以下为主程序
-snum = 6  # 6根火柴棒
+# 以下为主程序(改进版)
+snum = int(input("输入火柴棒数量："))  # 6根火柴棒
+if snum % 2 == 0:
+    send = "1" * (snum // 2)
+else:
+    send = "7" + "1" * (snum // 2 -1)
 print("你可以拼出这些数字：")
-for i in range(112):
+for i in range(int(send) + 1):
     if match_num(i) == snum:  # 如果i需要的火柴棒数等于现有火柴棒数
         print(i)
 
