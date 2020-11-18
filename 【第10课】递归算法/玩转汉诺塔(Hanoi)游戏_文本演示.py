af448a0c294b926b819d0a0b23bc0b7b04375184
hongm32 <XHM,1024cj>
2020-11-18 13:13:34 +0800

    修改《【第10课】递归算法》源码Hanoi拚写错误

--- 【第10课】递归算法/玩转汉诺塔(Hanoi)游戏_文本演示.py
+++ 【第10课】递归算法/玩转汉诺塔(Hanoi)游戏_文本演示.py
@@ -1,17 +1,17 @@
-# 玩转汉诺塔(Hanio)游戏_文本演示
+# 玩转汉诺塔(Hanoi)游戏_文本演示
 
 
-def hanno(k, s, m, t):
+def hanoi(k, s, m, t):
     # 定义一个函数,k层塔，将盘子从s借助m移动到t
     global Step
     if k == 1:
         Step += 1
         print(Step, s, '-->', t)  # 将一个盘子从s移动到t
     else:
-        hanno(k-1, s, t, m)  # 将前n-1个盘子从s移动到m上
+        hanoi(k-1, s, t, m)  # 将前n-1个盘子从s移动到m上
         Step += 1
         print(Step, s, '-->', t)  # 将最底下的最后一个盘子从s移动到t上
-        hanno(k-1, m, s, t)  # 将m上的n-1个盘子移动到t上
+        hanoi(k-1, m, s, t)  # 将m上的n-1个盘子移动到t上
 
 
 # 主程序
@@ -24,5 +24,5 @@ while True:
         break
 
 Step = 0
-hanno(n, 'A', 'B', 'C')
+hanoi(n, 'A', 'B', 'C')
 input("运行完毕，请按回车键退出...")
---
+++ 【第10课】递归算法/玩转汉诺塔(Hanoi)游戏_移动次数.py
@@ -0,0 +1,11 @@
+# 玩转汉诺塔(Hanoi)游戏_移动次数
+
+
+def hanoi(n):
+    if n == 1:
+        return 1
+    return 2 * hanoi(n - 1) + 1
+
+
+total = hanoi(64)
+print("需要移动{}次，约{}亿年".format(total, total/3600/24/365/1e8))
