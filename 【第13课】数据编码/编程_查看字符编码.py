d862dcb7ec5a8bfa0c68c501f6e4d31b307da1a9
hongm32 <XHM,1024cj>
2020-12-10 12:07:26 +0800

    增加《【第13课】数据编码》源码

--- 【第13课】数据编码/编程_查看字符编码.py
+++ 【第13课】数据编码/编程_查看字符编码.py
@@ -28,13 +28,14 @@ while True:
         #    1)对于单字节的符号，字节的第一位设为0，后面7位为这个符号的unicode码。因此对于英语字母，UTF-8编码和ASCII码是相同的。
         #    2)对于n字节的符号（n>1），第一个字节的前n位都设为1，第n+1位设为0，后面字节的前两位一律设为10。
         #      剩下的没有提及的二进制位，全部为这个符号的unicode码。
-        # Uncode符号范围      | UTF8编码方式
+        # Unicode符号范围      | UTF8编码方式
         # 0000 0000-0000 007F | 0xxxxxxx
         # 0000 0080-0000 07FF | 110xxxxx 10xxxxxx
         # 0000 0800-0000 FFFF | 1110xxxx 10xxxxxx 10xxxxxx
-        # 0001 0000-0010 FFFF | 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx 
+        # 0001 0000-0010 FFFF | 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
         str_unicode = ord(s)
         s_bin = "0" * 8 + bin(str_unicode)[2:]
+        str_utf = None
         if str_unicode <= 0x7F:
             str_utf = s_bin[-8:]
         elif str_unicode <= 0x7FF:
@@ -42,7 +43,7 @@ while True:
         elif str_unicode <= 0xFFFF:
             str_utf = "1110" + s_bin[-16:-12] + "10" + s_bin[-12:-6] + "10" + s_bin[-6:]
         elif str_unicode <= 0x10FFFF:
-            str_utf =  "11110" + s_bin[-21:-18] + "10" + s_bin[-18:-12] + "10" + s_bin[-12:-6] + "10" + s_bin[-6:]
+            str_utf = "11110" + s_bin[-21:-18] + "10" + s_bin[-18:-12] + "10" + s_bin[-12:-6] + "10" + s_bin[-6:]
         str_utf_code = hex(int(str_utf, 2)).replace("0x", "").upper()
         print("   UTF-8是{}H".format(str_utf_code))
         # 可以用以下方法string.encode()：
