83bd6512eebfbffd86ca2e447966190ff6090362
hongm32 <XHM,1024cj>
2021-03-01 08:56:55 +0800

    添加《【第20课】图形界面设计与编程》课件、源码及相关资料

--- 【第18-19课】加密与解密/数据安全性测试(任意字符).py
+++ 【第18-19课】加密与解密/数据安全性测试(任意字符).py
@@ -1,16 +1,17 @@
 from tkinter import *
+from tkinter import ttk
 import datetime
 import random
 import string
 
 
-class MyIterator():
+class MyIterator:
     # 单位字符集合
-    letters = string.ascii_lowercase + string.digits  # 小写字母+数字
     min_digits = 0
     max_digits = 0
 
-    def __init__(self, min_digits, max_digits):
+    def __init__(self, letters, min_digits, max_digits):
+        self.letters = letters
         # 实例化对象时给出密码位数范围，一般4到10位
         if min_digits < max_digits:
             self.min_digits = min_digits
@@ -26,7 +27,7 @@ class MyIterator():
     def __next__(self):
         rst = str()
         for item in range(0, random.randrange(self.min_digits, self.max_digits+1)):
-            rst += random.choice(MyIterator.letters)
+            rst += random.choice(self.letters)
         return rst
 
 
@@ -52,10 +53,27 @@ def time_to_hms(_time):
 def jiemi():
     d1 = datetime.datetime.now()  # 获取当前系统时间d1
     p = varin.get()  # 获取输入文本框的数字密码
+    idx = cmb.current()
+    if idx == 1:
+        letters = string.ascii_lowercase  # 小写字母
+    elif idx == 2:
+        letters = string.ascii_lowercase + string.digits  # 小写字母+数字
+    elif idx == 3:
+        letters = string.ascii_letters + string.digits  # 小写字母+数字
+    elif idx == 4:
+        letters = string.ascii_letters + string.digits + string.punctuation
+    else:
+        letters = string.digits  # 数字
+    for i in p:
+        if i not in letters:
+            varout.set("密码不合要求，重新输入")
+            varin.set("")
+            return
     if len(p) < 1 or len(p) >= 10:
-        varout.set("Error Input")
+        varout.set("密码位数过长，想等百年？")
+        varin.set("")
         return
-    for i in MyIterator(len(p), len(p)):
+    for i in MyIterator(letters, len(p), len(p)):
         if i == p:  # 如果密码相同
             d2 = datetime.datetime.now()  # 获取当前系统时间d2
             d = d2 - d1  # 取得时间差
@@ -69,11 +87,17 @@ root = Tk()
 root.geometry('300x100')
 root.title("数据安全性测试")
 frm = Frame(root)
+
 # left
 frm_L = Frame(frm)
 Label(frm_L, text='输入密码：', font=('Arial', 10)).pack()
 Label(frm_L, text='破解用时：', font=('Arial', 10)).pack()
 Label(frm_L, text='你的密码是：', font=('Arial', 10)).pack()
+cmb = ttk.Combobox(frm_L, width=12)
+cmb.pack()
+cmb['values'] = ('纯数字', '小写字母', '小写字母+数字', '字母+数字', '字母+数字+符号')
+# 设置默认值，即默认下拉框中的内容
+cmb.current(0)
 frm_L.pack(side=LEFT)
 
 # right
@@ -84,8 +108,8 @@ varout = StringVar()
 Entry(frm_R, show='*', textvariable=varin).pack()
 Entry(frm_R, textvariable=vartime, state="disabled").pack()
 Entry(frm_R, textvariable=varout, state="disabled").pack()
+Button(frm_R, text="破解", command=jiemi, relief="solid", width=10).pack()
 frm_R.pack(side=RIGHT)
 frm.pack()
 
-Button(root, text="破解", command=jiemi, relief="solid", width=10).pack()
 root.mainloop()
---
+++ 【第20课】图形界面/①教师-图形化.py
@@ -0,0 +1,12 @@
+﻿from tkinter import *       # 导入tkinter模块
+kaisa = Tk()                 # 建立一个窗口
+kaisa.title("凯撒密码")      # 设置窗口标题
+kaisa.geometry('300x200')    # 设置窗口大小
+
+Label(kaisa, text='请输入明文', font=('Arial', 10)).pack()
+mingwen=Text(kaisa,width=300,height=4)
+mingwen.pack()
+mingwen.focus_set()    # 获得焦点
+Button(kaisa, text="加密",relief="solid",width=10).pack()
+
+kaisa.mainloop()
