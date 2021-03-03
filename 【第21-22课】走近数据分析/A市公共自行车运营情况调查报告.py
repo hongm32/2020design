ed126c00a46ea1e086bd82fdb00fa2e8a9fc9136
hongm32 <XHM,1024cj>
2021-03-03 12:01:40 +0800

    添加《【第22课】走近数据分析》源码和相关资料

--- 【第21-22课】走近数据分析/A市公共自行车运营情况调查报告.py
+++ 【第21-22课】走近数据分析/A市公共自行车运营情况调查报告.py
@@ -1,6 +1,8 @@
+import csv
 import matplotlib
 import matplotlib.pyplot as plt
-import csv
+import numpy as np
+
 
 # 指定默认字体
 matplotlib.rcParams['font.sans-serif'] = ['SimHei']
@@ -15,33 +17,41 @@ with open(file, encoding="GBK") as f:
         if "GXZX" in row[0]:
             for idx, value in enumerate(row):
                 if idx == 0:
-                    data[idx].append(value[-2:])
+                    data[idx].append(value)
                 else:
                     data[idx].append(int(value))
             data[5].append(round(data[2][-1] / data[1][-1], 1))
             data[6].append(round(data[3][-1] / data[1][-1], 1))
             data[7].append(round(data[4][-1] / data[3][-1] * 100, 1))
-#  print(data)
+    for i in range(1, 8):
+        data[i] = np.asarray(data[i])
+    data.append(np.arange(len(data[0])))
 
-plt.figure(figsize=(12, 9))  # 自定义画布大小，单位英寸
+# 自定义画布大小，单位英寸
+plt.figure(figsize=(12, 12))
 
 # A市自行车借还数量对比图
 plt.subplot(3,  1,  1)  # 建立subplot网格，高为3，宽为1，激活第一个子图
-plt.bar(data[0], data[2], label="借数量")
-plt.bar(data[0], data[3], bottom=data[2], label="还数量")
+plt.bar(data[-1], data[2], label="借数量")
+plt.bar(data[-1]+ 0.3, data[3], label="还数量")
+plt.xticks(data[-1], data[0], rotation=270)  # 坐标轴标签替换，rotaion旋转
 plt.legend(loc='best')  # 图例，best：自动选择最佳位置 upper center：上部居中等
 plt.title('A市自行车借还数量对比图', size=20)
 
 # A市自行车平均毎桩借还数图
 plt.subplot(3,  1,  2)
-plt.bar(data[0], data[5], label="平均每桩借车数")
-plt.bar(data[0], data[6], bottom=data[5], label="平均每桩还车数")
+plt.bar(data[-1], data[5], label="平均每桩借车数")
+plt.bar(data[-1] + 0.4, data[6], label="平均每桩还车数")
+plt.plot(data[-1] + 0.2, -data[5])
+plt.plot(data[-1] + 0.2, -data[6])
+plt.xticks(data[-1], data[0], rotation=270)  # 坐标轴标签替换，rotaion旋转
 plt.legend(loc='best')  # 图例，best：自动选择最佳位置 upper center：上部居中等
 plt.title('A市自行车平均毎桩借还数图', size=20)
 
 # A市自行车还车数与维修量比图
 plt.subplot(3,  1,  3)
-plt.bar(data[0], data[7], label="维修数量与还车数量比")
+plt.bar(data[-1], data[7], label="维修数量与还车数量比")
+plt.xticks(data[-1], data[0], rotation=270)  # 坐标轴标签替换，rotaion旋转
 plt.legend(loc='best')  # 图例，best：自动选择最佳位置 upper center：上部居中等
 plt.title('A市自行车还车数与维修量比图', size=20)
 
